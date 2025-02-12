9ab7996f421dc8d6f928160aeafab475860fe1eb
Brett Lintgen <blintgen@gmail.com>
2025-02-12 08:02:10 -0600

    wrote questions array, wrote the inquirer prompt function, minor fixes to make it all work together

--- Develop/index.js
+++ Develop/index.js
@@ -1,14 +1,72 @@
 // TODO: Include packages needed for this application
 import inquirer from 'inquirer'
 import fs from 'fs'
+import generateMarkdown from './utils/generateMarkdown.js';
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = ["What is the name you would like to attach to this project?", "How would someone contact you if they have question, comments, etc.?", "What is the title of the project?", "What is the project for?", "How would someone install this project?", "How is this project used once installed?", "What is the license you're using?", "What's the link to that License"];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, (err) => 
+        err ? console.error(err) : console.log("README generated"))
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt([
+        {
+            name: "name",
+            message: questions[0]
+        },
+        {
+            name: "contact",
+            message: questions[1]
+        },
+        {
+            name: "title",
+            message: questions[2]
+        },
+        {
+            name: "description",
+            message: questions[3]
+        },
+        {
+            name: "installation",
+            message: questions[4]
+        },
+        {
+            name: "usage",
+            message: questions[5]
+        },
+        {
+            name: "license",
+            message: questions[6]
+        },
+        {
+            name: "link",
+            message: questions[7]
+        }
+
+    ]).then((response) => 
+        {
+            const data = 
+            {
+                name: response.name,
+                contact: response.contact,
+                title: response.title,
+                description: response.description,
+                installation: response.installation,
+                usage: response.usage,
+                license:
+                {
+                    name: response.license,
+                    link: response.link
+                }
+            }
+            console.log(data)
+            writeToFile("README.md", generateMarkdown(data))
+        })
+}
 
 // Function call to initialize app
 init();
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -2,7 +2,7 @@
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
   if (license.length != 0){
-    return license
+    return `# ${license}\n`
   }
   else {
     return ""
@@ -13,7 +13,7 @@ function renderLicenseBadge(license) {
 // If there is no license, return an empty string
 function renderLicenseLink(license) {
   if (license.length != 0){
-    return license
+    return `# ${license}\n`
   }
   else {
     return ""
@@ -23,14 +23,14 @@ function renderLicenseLink(license) {
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(license) {
-  renderLicenseBadge(license.name)
-  renderLicenseLink(license.link)
+  console.log(license)
+  return `${renderLicenseBadge(license.name)} ${renderLicenseLink(license.link)}`
 }
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `
-# ${data.title}
+  console.log(data)
+  return `# ${data.title}###${renderLicenseSection(data.license)}
 
 ## ${data.description}
 
@@ -38,10 +38,8 @@ function generateMarkdown(data) {
 
 ## ${data.usage}
 
-## ${renderLicenseSection(data.license)}
 
-
-
+### for issues contact ${data.name} at ${data.contact}
 `
 }
 
